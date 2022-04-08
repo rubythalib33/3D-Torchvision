0626bdf17ce789e738883bf4dc52a0b3e629ba05
rubythalib33 <rubyabdullah14@gmail.com>
2022-04-08 12:29:12 +0700

    update mobilenetv2_3d

--- torchvision_3d/models/__init__.py
+++ torchvision_3d/models/__init__.py
@@ -3,3 +3,4 @@ from torchvision_3d.models.vgg import *
 from torchvision_3d.models.resnet import *
 from torchvision_3d.models.densenet import *
 from torchvision_3d.models.squeezenet import *
+from torchvision_3d.models.mobilenetv2 import *
